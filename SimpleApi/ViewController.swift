645ff1523e4777030cb24cdfb09ba005c85eb609
LLltirlec <fhntvrf1998@gmail.com>
2023-06-19 22:25:24 +0300

    Initial Commit

---
+++ SimpleApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleApi
+//
+//  Created by Евгений Ефимов on 19.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
